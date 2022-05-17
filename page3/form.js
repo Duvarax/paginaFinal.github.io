b72b1a694daaa66c0478664665bf82a9db12ef7b
Duvara Claudio <91039016+Duvarax@users.noreply.github.com>
2022-05-17 19:32:33 -0300

    Add files via upload

---
+++ page3/form.js
@@ -0,0 +1,99 @@
+var indice = 0;
+var img = new Array;
+img[0] = "img/Cerberus.png";
+img[1] = "img/Charon.png";
+img[2] = "img/Hypnos.png";
+img[3] = "img/Nyx.png";
+img[4] = "img/Skelly.png";
+function cambiarAtras1() {
+    indice--;
+    if (indice == -1) {
+        indice = 4;
+    }
+    document.getElementById("slider").src = img[indice];
+}
+function cambiarAdelante1() {
+    indice++;
+    if (indice == 5) {
+        indice = 0;
+    }
+    document.getElementById("slider").src = img[indice];
+}
+
+setInterval(cambiarAdelante1, 5000);
+
+var img1 = new Array;
+img1[0] = "img/1.png";
+img1[1] = "img/2.png";
+img1[2] = "img/3.png";
+img1[3] = "img/4.png";
+img1[4] = "img/5.png";
+img1[5] = "img/6.png";
+img1[6] = "img/7.png";
+
+var i = 0;
+function cambiarAtras2() {
+    i--;
+    if (i == -1) {
+        i = 6;
+    }
+    document.getElementById("slider1").src = img1[i];
+}
+function cambiarAdelante2() {
+    i++;
+    if (i == 7) {
+        i = 0;
+    }
+    document.getElementById("slider1").src = img1[i];
+}
+
+setInterval(cambiarAtras2, 5000);
+
+var img2 = new Array;
+img2[0] = "img/G.png";
+img2[1] = "img/H.png";
+img2[2] = "img/P.png";
+img2[3] = "img/A.png";
+
+var j = 0;
+function cambiarAtras3() {
+    j--;
+    if (j == -1) {
+        j = 3;
+    }
+    document.getElementById("slider2").src = img2[j];
+}
+function cambiarAdelante3() {
+    j++;
+    if (j == 4) {
+        j = 0;
+    }
+    document.getElementById("slider2").src = img2[j];
+}
+setInterval(cambiarAtras3, 5000);
+
+function eliminar() {
+    var nombre = document.forms["formulario"]["nombre"].value;
+    var correo = document.forms["formulario"]["correo"].value;
+    var edad = document.forms["formulario"]["edad"].value;
+    if (nombre == "" || correo == "" || isNaN(edad)) {
+        window.alert("Debe rellenar todos los campos");
+        if(isNaN(edad)){
+            alert("Edad debe ser un numero");
+        }
+    } else if(isNaN(nombre) && isNaN(correo)) {
+        //Elimino la seccion que quiero
+        var objeto = document.getElementById("form-div");
+        objeto.parentNode.removeChild(objeto);
+        //Creo un contenedor
+        var nuevo = document.createElement("div");
+        nuevo.className = "despues";
+        //Agrego texto
+        var texto = document.createTextNode(" Solicitud enviada! ");
+        nuevo.appendChild(texto);
+        //Agrego el contenedor con el texto a un elemento padre
+        var padre = document.getElementById("padre");
+        padre.className = "padre-despues";
+        padre.appendChild(nuevo);
+    }
+}
